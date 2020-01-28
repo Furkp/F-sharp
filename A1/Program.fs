157e00074b2ab88a1de0e613836ef27fab8fe791
fuka <fuka@itu.dk>
2020-01-28 09:29:41 +0100

    :

---
+++ A1/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+module misc =
+    let sqr x = x*x
+
+[<EntryPoint>]
+let main argv =
+    printfn "sqr 2 = %d" (misc.sqr 2)
+    0 // return an integer exit code
