844814d7e018a11ce3a77afa07044c2b5450cc5d
fuka <fuka@itu.dk>
2020-04-29 12:26:31 +0200

    .

---
+++ Scrabble/NutBot/Eval.fs
@@ -0,0 +1,315 @@
+﻿module Eval
+
+    open StateMonad
+
+    (* Code for testing *)
+    
+    let hello = [
+        ('H', 4)
+        ('E', 1)
+        ('L', 1)
+        ('L', 1)
+        ('O', 1)]
+
+    let state = mkState [("x", 5); ("y", 42)] hello ["_pos_"; "_result_"]
+    
+    let emptyState = mkState [] [] []
+
+    let add a b = 
+        a >>= (fun v1 -> 
+            b >>= (fun v2 -> ret (v1+v2)))
+
+    let div a b = 
+        a >>= (fun v1 -> 
+            b >>= (fun v2 -> 
+                match v2 with
+                | 0 -> fail DivisionByZero
+                | n -> ret (v1/v2)))
+     
+    type aExp =
+        | N of int
+        | V of string
+        | WL
+        | PV of aExp
+        | Add of aExp * aExp
+        | Sub of aExp * aExp
+        | Mul of aExp * aExp
+        | Div of aExp * aExp
+        | Mod of aExp * aExp
+        | CharToInt of cExp
+
+    and cExp =
+       | C  of char  (* Character value *)
+       | CV of aExp  (* Character lookup at word index *)
+       | ToUpper of cExp
+       | ToLower of cExp
+       | IntToChar of aExp
+
+    type bExp =             
+       | TT                   (* true *)
+       | FF                   (* false *)
+
+       | AEq of aExp * aExp   (* numeric equality *)
+       | ALt of aExp * aExp   (* numeric less than *)
+
+       | Not of bExp          (* boolean not *)
+       | Conj of bExp * bExp  (* boolean conjunction *)
+
+       | IsVowel of cExp      (* check for vowel *)
+       | IsConsonant of cExp  (* check for constant *)
+
+    let (.+.) a b = Add (a, b)
+    let (.-.) a b = Sub (a, b)
+    let (.*.) a b = Mul (a, b)
+    let (./.) a b = Div (a, b)
+    let (.%.) a b = Mod (a, b)
+
+    let (~~) b = Not b
+    let (.&&.) b1 b2 = Conj (b1, b2)
+    let (.||.) b1 b2 = ~~(~~b1 .&&. ~~b2)       (* boolean disjunction *)
+    let (.->.) b1 b2 = (~~b1) .||. b2           (* boolean implication *) 
+       
+    let (.=.) a b = AEq (a, b)   
+    let (.<.) a b = ALt (a, b)   
+    let (.<>.) a b = ~~(a .=. b)
+    let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
+    let (.>=.) a b = ~~(a .<. b)                (* numeric greater than or equal to *)
+    let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)    
+
+    let sub a b = 
+        a >>= (fun v1 -> 
+            b >>= (fun v2 -> ret (v1-v2)))
+
+    let mul a b = 
+        a >>= (fun v1 -> 
+            b >>= (fun v2 -> ret (v1*v2)))
+
+    let md a b = 
+        a >>= (fun v1 -> 
+            b >>= (fun v2 -> 
+                match v2 with
+                | 0 -> fail DivisionByZero
+                | n -> ret (v1/v2)))
+            
+    let isVowel c = 
+        match System.Char.ToLower(c) with
+        | 'a' |'e' |'i' | 'o' | 'u' -> true
+        | _ -> false
+
+    let isConsonant c =
+        System.Char.IsLetter(c) && not (isVowel(c))
+
+    let rec arithEval a : SM<int> = 
+        match a with
+            | N x -> ret x
+            | V x -> lookup x
+            | WL -> wordLength 
+            | PV x -> arithEval x >>= pointValue
+            | Add(x, y) -> add (arithEval x) (arithEval y)
+            | Sub(x, y) -> sub (arithEval x) (arithEval y)
+            | Mul(x, y) -> mul (arithEval x) (arithEval y)
+            | Div(x, y) -> div (arithEval x) (arithEval y)
+            | Mod(x, y) -> md (arithEval x) (arithEval y)
+            | CharToInt(x) -> (charEval x) >>= (fun v -> ret (int v)) 
+    
+    and charEval c : SM<char> = 
+        match c with
+            | C(x) -> ret x
+            | CV(x) -> arithEval x >>= characterValue
+            | ToUpper(x) -> charEval x >>= (fun v -> ret (System.Char.ToUpper v))
+            | ToLower(x) -> charEval x >>= (fun v -> ret (System.Char.ToLower v))
+            | IntToChar(x) -> arithEval x >>= (fun v -> ret (char v))
+   
+    and boolEval b : SM<bool> = 
+        match b with 
+            | TT -> ret true               
+            | FF -> ret false    
+            | AEq(x, y) -> 
+                arithEval x >>= (fun v1 -> 
+                    arithEval y >>= (fun v2 -> ret (v1 = v2)))
+            | ALt(x, y) -> 
+                arithEval x >>= (fun v1 -> 
+                    arithEval y >>= (fun v2 -> ret (v1 < v2)))
+            | Not(x) -> boolEval x >>= fun v -> ret (not v)     
+            | Conj(x, y) -> 
+                boolEval x >>= (fun v1 -> 
+                    boolEval y >>= (fun v2 -> ret (v1 && v2)))
+            | IsVowel(x) -> charEval x >>= fun v -> ret (isVowel v)
+            | IsConsonant(x) -> charEval x >>= fun v -> ret (isConsonant v)
+
+    type stm =                (* statements *)
+    | Declare of string       (* variable declaration *)
+    | Ass of string * aExp    (* variable assignment *)
+    | Skip                    (* nop *)
+    | Seq of stm * stm        (* sequential composition *)
+    | ITE of bExp * stm * stm (* if-then-else statement *)
+    | While of bExp * stm     (* while statement *)
+
+    let rec stmntEval stmnt : SM<unit> = 
+        match stmnt with
+            | Declare(x) -> declare x
+            | Skip -> ret ()
+            | Ass(x, a) -> arithEval a >>= update x
+            | Seq(stm1, stm2) -> stmntEval stm1 >>>= stmntEval stm2
+            | ITE(guard, stm1, stm2) -> boolEval guard >>= (fun v -> 
+                    if(v) then
+                        push >>>= stmntEval stm1 >>>= pop
+                    else
+                        push >>>= stmntEval stm2 >>>= pop)
+            | While(guard, stm) -> boolEval guard >>= (fun v -> 
+                    if(v) then
+                        push >>>= stmntEval stm >>>= stmntEval (While(guard, stm)) >>>= pop
+                    else
+                        ret ())
+
+(*
+(* Part 3 (Optional) *)
+*)
+    type StateBuilder() =
+
+        member this.Bind(f, x)    = f >>= x
+        member this.Return(x)     = ret x
+        member this.ReturnFrom(x) = x
+        member this.Delay(f)      = f ()
+        member this.Combine(a, b) = a >>= (fun _ -> b)
+        
+    let prog = new StateBuilder()
+
+    let rec arithEval2 a = 
+        match a with
+            | N x -> prog { return x }
+            | V x -> prog { return! lookup x }
+            | WL -> prog { return! wordLength }
+            | PV x -> prog { 
+                let! ax = arithEval2 x 
+                return! pointValue ax }
+            | Add(x, y) -> prog { return! add (arithEval2 x) (arithEval2 y) }
+            | Sub(x, y) -> prog { return! sub (arithEval2 x) (arithEval2 y) }
+            | Mul(x, y) -> prog { return! mul (arithEval2 x) (arithEval2 y) }
+            | Div(x, y) -> prog { return! div (arithEval2 x) (arithEval2 y) }
+            | Mod(x, y) -> prog { return! md (arithEval2 x) (arithEval2 y) }
+            | CharToInt(x) -> prog {
+                let! c = charEval2 x
+                return int (c)
+            }
+
+    and charEval2 c = 
+        match c with
+            | C(x) -> prog { return x }
+            | CV(x) -> prog {
+                let! a = arithEval2 x
+                return! characterValue a}
+            | ToUpper(x) -> prog {
+                let! c = charEval x
+                return System.Char.ToUpper c }
+            | ToLower(x) -> prog {
+                let! c = charEval x
+                return System.Char.ToLower c }
+            | IntToChar(x) -> prog {
+                let! c = arithEval2 x
+                return char c }
+            
+    and boolEval2 b = 
+            match b with 
+                | TT -> prog { return true }               
+                | FF -> prog { return false }    
+                | AEq(x, y) ->  prog { 
+                    let! ax = arithEval2 x
+                    let! ay = arithEval2 y 
+                    return (ax = ay) }
+                | ALt(x, y) -> prog { 
+                    let! ax = arithEval2 x
+                    let! ay = arithEval2 y 
+                    return (ax < ay) }
+                | Not(x) -> prog { 
+                    let! b = boolEval2 x
+                    return not b }
+                | Conj(x, y) -> prog { 
+                    let! ax = boolEval2 x
+                    let! ay = boolEval2 y 
+                    return (ax && ay) }
+                | IsVowel(x) -> prog { 
+                    let! c = charEval2 x
+                    return isVowel c }
+                | IsConsonant(x) -> prog { 
+                    let! c = charEval2 x
+                    return isConsonant c }
+
+
+    let rec stmntEval2 stm : SM<unit> = 
+        match stm with
+            | Declare(x) -> prog { do! declare x }  
+            | Skip -> prog { return () }  
+            | Ass(x, a) -> prog { 
+                let! av = arithEval2 a
+                do! update x av
+             }  
+            | Seq(stm1, stm2) -> prog { 
+                do! stmntEval2 stm1 
+                do! stmntEval2 stm2 }
+            | ITE(guard, stm1, stm2) -> prog {
+                let! b = boolEval2 guard
+                if(b) then
+                    do! push
+                    do! stmntEval2 stm1 
+                    do! pop
+                else
+                    do! push
+                    do! stmntEval2 stm2 
+                    do! pop}
+            | While(guard, stm) -> prog {
+                let! b = boolEval2 guard
+                if(b) then
+                    do! push
+                    do! stmntEval2 stm
+                    do! stmntEval2 <| While(guard, stm)
+                    do! pop
+                else
+                    return ()}
+
+(* Part 4 (Optional) *) 
+
+    type word = (char * int) list
+    type squareFun = word -> int -> int -> int
+
+    let stmntToSquareFun stm : squareFun = 
+        (fun w pos acc -> 
+            (stmntEval2 stm) >>>= lookup "_result_" |> 
+            evalSM (mkState [("_pos_", pos); ("_acc_", acc); ("_result_", 0)] w ["_pos_"; "_acc_"; "_result_"]) |>
+            getResult)
+        
+    type coord = int * int
+
+    // type boardFun = coord -> Result<squareFun option, Error> 
+    type boardFun = coord -> Map<int, squareFun> option
+    
+    let stmntToBoardFun stm (m: Map<int, Map<int, squareFun>>) : boardFun = 
+        (fun (x,y) ->
+            prog{
+                do! stmntEval2 stm
+                let! id = lookup "_result_"
+                return m.TryFind id
+            } |> 
+                evalSM (mkState [("_x_", x); ("_y_", y); ("_result_", 0)] [] ["_x_"; "_y_"; "_result_"]) |> function
+                | Success x -> x
+                | Failure err -> failwith (sprintf "Error: %A" err)) 
+        
+                
+    
+    type board = {
+        center        : coord
+        defaultSquare : squareFun
+        squares       : boardFun
+        placed        : Map<int * int, (int * char)>
+    }
+
+    type squareProg = Map<int, string>
+    type boardProg = {
+        prog       : stm;
+        squares    : Map<int, Map<int, squareFun>>
+    }
+
+    let mkBoard c defaultSq boardStmnt ids = 
+       { center = c;
+         defaultSquare = stmntToSquareFun defaultSq;
+         squares = stmntToBoardFun boardStmnt (List.map (fun (x, y) -> (x, stmntToSquareFun y)) ids |> Map.ofList)}
---
+++ Scrabble/NutBot/Parser.fs
@@ -0,0 +1,222 @@
+﻿module Ass7
+
+open System
+open FParsec
+open ScrabbleUtil
+
+type aExp =
+    | N of int              (* Integer literal *)
+    | V of string           (* Variable reference *)
+
+    | WL                    (* Word length *)
+    | PV of aExp            (* Point value lookup at word index *)
+
+    | Add of aExp * aExp    (* Addition *)
+    | Sub of aExp * aExp    (* Subtraction *)
+    | Mul of aExp * aExp    (* Multiplication *)
+    | Div of aExp * aExp    (* Division *)
+    | Mod of aExp * aExp    (* Modulo *)
+
+    | CharToInt of cExp     (* Cast to integer *)
+
+and cExp =
+    | C  of char             (* Character literal *)
+    | CV of aExp             (* Character lookup at word index *)
+
+    | ToUpper of cExp        (* Convert character to upper case *)
+    | ToLower of cExp        (* Convert character to lower case *)
+
+    | IntToChar of aExp      (* Cast to character *)
+
+type bExp =
+    | TT                   (* True *)
+    | FF                   (* False *)
+
+    | AEq of aExp * aExp   (* Numeric equality *)
+    | ALt of aExp * aExp   (* Numeric less than *)
+
+    | Not of bExp          (* Boolean not *)
+    | Conj of bExp * bExp  (* Boolean conjunction *)
+
+    | IsVowel of cExp      (* Check for vowel *)
+    | IsConsonant of cExp  (* Check for constant *)
+
+type stm =
+    | Declare of string       (* NEW: Variable declaration *)
+    | Ass of string * aExp    (* variable assignment *)
+    | Skip                    (* Nop *)
+    | Seq of stm * stm        (* Sequential composition *)
+    | ITE of bExp * stm * stm (* If-Then-Else statement *)
+    | While of bExp * stm     (* While statement *)
+
+module ImpParser =
+    let (.+.) a b = Add (a, b)
+    let (.-.) a b = Sub (a, b)
+    let (.*.) a b = Mul (a, b)
+    let (./.) a b = Div (a, b)
+    let (.%.) a b = Mod (a, b)
+
+    let (~~) b = Not b
+    let (.&&.) b1 b2 = Conj (b1, b2)
+    let (.||.) b1 b2 = ~~(~~b1 .&&. ~~b2)       (* boolean disjunction *)
+
+    let (.=.) a b = AEq (a, b)
+    let (.<.) a b = ALt (a, b)
+    let (.<>.) a b = ~~(a .=. b)                (* numeric inequality *)
+    let (.<=.) a b = a .<. b .||. ~~(a .<>. b)  (* numeric smaller than or equal to *)
+    let (.>=.) a b = ~~(a .<. b)                (* numeric greater than or equal to *)
+    let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)
+
+    let whitespaceChar = satisfy Char.IsWhiteSpace
+    let pAnyChar = anyChar
+    let letterChar = asciiLetter
+    let alphaNumeric = asciiLetter <|> digit
+    let charListToStr charList = String(List.toArray charList) |> string
+    let pint = pint32
+    let choice ps = ps |> Seq.map attempt |> choice
+    let (<|>) p1 p2 = attempt p1 <|> attempt p2
+
+    let pIntToChar = pstring "intToChar"
+    let pPointValue = pstring "pointValue"
+    let pCharToInt = pstring "charToInt"
+    let pToUpper = pstring "toUpper"
+    let pToLower = pstring "toLower"
+    let pCharValue = pstring "charValue"
+    let pTrue = pstring "true"
+    let pFalse = pstring "false"
+    let pif = pstring "if"
+    let pthen = pstring "then"
+    let pelse = pstring "else"
+    let pwhile = pstring "while"
+    let pdo = pstring "do"
+    
+    let (.>*>.) p1 p2 =
+        p1 .>> spaces >>= (fun r1 -> p2 >>= fun r2 -> preturn (r1, r2)) 
+
+    let (.>*>) p1 p2 = p1 .>*>. p2 |>> fst 
+    let (>*>.) p1 p2 = p1 .>*>. p2 |>> snd
+
+    let parenthesise p = pchar '(' >*>. p .>*> pchar ')'
+
+    let pid = letterChar .>>. (many alphaNumeric |>> charListToStr) |>> fun (a, b) -> string a+b
+
+    let unop op a = op >*>. a
+
+    let binop op a b = a .>*> op .>*>. b
+
+    let TermParse, tref = createParserForwardedToRef<aExp, unit>()
+    let ProdParse, pref = createParserForwardedToRef<aExp, unit>()
+    let AtomParse, aref = createParserForwardedToRef<aExp, unit>()
+
+    let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
+
+    let SubParse = binop (pchar '-') ProdParse TermParse |>> Sub <?> "Sub"
+    
+    let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
+
+    let DivParse = binop (pchar '/') AtomParse ProdParse |>> Div <?> "Div"
+
+    let ModParse = binop (pchar '%') AtomParse ProdParse |>> Mod <?> "Mod"
+
+    let NegParse = unop (pchar '-') AtomParse  |>> (fun a -> Mul (N(-1), a)) <?> "Neg"
+
+    let WLParse = pstring "wordLength" >*>. pid |>> (fun a -> N a.Length) <?> "WL"
+    
+    let PVParse = pPointValue >*>. AtomParse |>> PV <?> "PV"
+    
+    let VParse = pid |>> V <?> "Var"
+    
+    let NParse   = pint |>> N <?> "Int"   
+    
+    let ParParse = parenthesise TermParse
+
+    do tref := choice [AddParse; SubParse; ProdParse]
+    do pref := choice [MulParse; DivParse; ModParse; AtomParse]
+
+    let AexpParse = TermParse
+
+    let CharParse, cref = createParserForwardedToRef<cExp, unit>()
+
+    let CParse = pchar ''' >>. pAnyChar .>> pchar ''' |>> C <?> "Char"
+
+    let CVParse = pCharValue >*>. parenthesise TermParse |>> CV <?> "CV"
+    
+    let TUParse = pToUpper >*>. parenthesise CharParse |>> ToUpper <?> "ToUpper"
+
+    let TLParse = pToLower >*>. parenthesise CharParse |>> ToLower <?> "ToLower"
+
+    let ITCParse = pIntToChar >*>. parenthesise TermParse |>> IntToChar <?> "IntToChar"
+
+    let CTIParse = pCharToInt >*>. parenthesise CharParse |>> CharToInt <?> "CharToInt"
+
+    do cref := choice [CVParse; TUParse; TLParse; ITCParse; CParse]
+    
+    do aref := choice [WLParse; PVParse; CTIParse; VParse; NegParse; NParse; ParParse]
+
+    let CexpParse = CharParse
+
+    let BoolParse, bref = createParserForwardedToRef<bExp, unit>()
+    let Bool2Parse, b2ref = createParserForwardedToRef<bExp, unit>()
+    let Bool3Parse, b3ref = createParserForwardedToRef<bExp, unit>()
+
+    let TParse = pstring "true" |>> (fun a -> TT) <?> "True"
+    let FParse = pstring "false" |>> (fun a -> FF) <?> "False"
+
+    let AndParse = Bool2Parse .>*> pstring "/\\" .>*>. BoolParse |>> Conj <?> "Conj"
+    let OrParse =  Bool2Parse .>*> pstring "\\/" .>*>. BoolParse |>> (fun (a, b) -> Not(a), Not(b)) |>> Conj |>> Not <?> "Disj"
+
+    let EqParse =  AtomParse .>*> pstring "=" .>*>. TermParse |>> AEq <?> "AEq"
+    let NEqParse =  AtomParse .>*> pstring "<>" .>*>. TermParse |>> AEq |>> Not <?> "Not Equal"
+    let LTParse =  AtomParse .>*> pstring "<" .>*>. TermParse |>> ALt <?> "Less than"
+    let LTEParse =  AtomParse .>*> pstring "<=" .>*>. TermParse |>> (fun a -> (a |> ALt |> Not), (a |> AEq |> Not |> Not |> Not)) |>> Conj |>> Not <?> "Less than/Equal"
+    let GTParse =  AtomParse .>*> pstring ">" .>*>. TermParse |>> (fun a -> ((a |> AEq |> Not), (a |> ALt |> Not))) |>> Conj  <?> "Greater than"
+    let GTEParse =  AtomParse .>*> pstring ">=" .>*>. TermParse |>> ALt |>> Not <?> "Greater than/Equal"
+    
+    let NotParse = pstring "~" >>. BoolParse |>> Not <?> "Not"
+
+    let BParse = parenthesise BoolParse
+
+    do bref := choice [AndParse; OrParse; Bool2Parse]
+    do b2ref := choice [EqParse; NEqParse; LTParse; LTEParse; GTParse; GTEParse; Bool3Parse]
+    do b3ref := choice [NotParse; BParse; TParse; FParse]
+
+    let BexpParse = BoolParse
+
+    let StmntParse, sref = createParserForwardedToRef<stm, unit>()
+    let Stmnt2Parse, s2ref = createParserForwardedToRef<stm, unit>()
+
+    let AssParse = pid .>*> pstring ":=" .>*>. TermParse |>> Ass <?> "Ass"
+
+    let DecParse = pstring "declare" >>. whitespaceChar >*>. pid |>> Declare <?> "Dec" 
+
+    let SeqParse = Stmnt2Parse .>*> pchar ';' .>*>. StmntParse |>> Seq <?> "Seq"
+
+    let bracketise p = pchar '{' >*>. p .>*> pchar '}'
+
+    let ITEParse = pstring "if" >*>. BoolParse .>*> pstring "then" .>*>. 
+                    bracketise StmntParse .>*> pstring "else" .>*>. 
+                    bracketise StmntParse |>> (fun ((a,b), c) -> (a,b,c) |> ITE) <?> "ITE"
+
+    let ITParse = pstring "if" >*>. BoolParse .>*> pstring "then" .>*>. 
+                    bracketise StmntParse |>> (fun (a, b) -> (a,b,Skip) |> ITE) <?> "IT"
+
+    let WhParse = pstring "while" >*>. BoolParse .>*> pstring "do" .>*>. 
+                    bracketise StmntParse |>> While <?> "While"
+
+    do sref := choice [SeqParse; Stmnt2Parse]
+    do s2ref := choice [ITEParse; ITParse; WhParse; AssParse; DecParse]
+    let stmParse = StmntParse 
+
+    let getParserResult s (pr : ParserResult<'a, 'b>) =
+        match pr with
+        | Success (t, _, _)   -> 
+             t
+        | Failure (err, _, _) -> 
+            let errorStr = sprintf "Failed to parse %s\n\nError:\n%A" s err
+            DebugPrint.debugPrint errorStr
+            failwith errorStr
+
+    let runTextParser parser text =
+        match runParserOnString parser () "" text with
+        | Success (stm, _, _) -> stm
+        | Failure (err, _, _) -> failwith err  
